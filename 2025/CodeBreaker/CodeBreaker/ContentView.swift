88b040622eca4c82d6cf2798668fa237a916288f
Alvaro <alvher.orellana@gmail.com>
2026-08-30 11:31:52 -0400

    Code is as is at the end of lecture 2

--- 2025/CodeBreaker/CodeBreaker/ContentView.swift
+++ 2025/CodeBreaker/CodeBreaker/ContentView.swift
@@ -10,13 +10,55 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            pegs(for: [.blue, .red, .yellow, .orange])
+            pegs(for: [.blue, .red, .yellow, .pink])
+            pegs(for: [.blue, .red, .yellow, .primary])
         }
         .padding()
     }
+    
+    func pegs(for colors: [Color]) -> some View {
+        HStack {
+            ForEach(colors.indices, id: \.self) { index in
+                Circle().foregroundStyle(colors[index])
+            }
+            MatchMarkers(markers: [.exact, .inexact, .inexact])
+        }
+    }
+}
+
+struct MatchMarkers: View {
+    let markers: [Match]
+    
+    var body: some View {
+        VStack {
+            HStack {
+                matchMarker(pegNumber: 0)
+                matchMarker(pegNumber: 1)
+            }
+            HStack {
+                matchMarker(pegNumber: 2)
+                matchMarker(pegNumber: 3)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func matchMarker(pegNumber: Int) -> some View {
+        let exactCount = markers.count(where: { $0 == .exact})
+        let foundCount = markers.count(where: { $0 != .noMatch})
+        
+        Circle()
+            .fill(exactCount > pegNumber ? Color.primary : Color.clear)
+            .strokeBorder(foundCount > pegNumber ? Color.pink : Color.clear, lineWidth: 2)
+            .aspectRatio(1, contentMode: .fit)
+    }
+}
+
+enum Match {
+    case exact
+    case inexact
+    case noMatch
 }
 
 #Preview {
