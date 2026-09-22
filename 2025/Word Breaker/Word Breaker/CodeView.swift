ab07e82f46326433c321328c9f3f56ca70c45890
Alvaro <alvher.orellana@gmail.com>
2026-09-22 18:52:15 -0300

    refactor

---
+++ 2025/Word Breaker/Word Breaker/CodeView.swift
@@ -0,0 +1,71 @@
+//
+//  CodeView.swift
+//  Word Breaker
+//
+
+import SwiftUI
+
+struct CodeView<AncillaryView>: View where AncillaryView: View {
+    let code: Code
+    @Binding var selection: Int
+    @ViewBuilder let ancillaryView: () -> AncillaryView
+
+    init(
+        code: Code,
+        selection: Binding<Int> = .constant(-1),
+        @ViewBuilder ancillaryView: @escaping () -> AncillaryView = { EmptyView() }
+    ) {
+        self.code = code
+        self._selection = selection
+        self.ancillaryView = ancillaryView
+    }
+
+    var body: some View {
+        HStack {
+            let characters = Array(code.word)
+            ForEach(characters.indices, id: \.self) { index in
+                Letter.shape
+                    .fill(pegColor(at: index))
+                    .aspectRatio(Letter.aspectRatio, contentMode: .fit)
+                    .overlay {
+                        Text(characters[index].description)
+                            .font(.largeTitle)
+                            .foregroundStyle(Color(.systemBackground))
+                    }
+                    .padding(Letter.selectionPadding)
+                    .background {
+                        if code.kind == .guess && selection == index {
+                            Letter.shape.foregroundStyle(Letter.selectionColor)
+                        }
+                    }
+                    .onTapGesture {
+                        selection = index
+                    }
+            }
+            Color.clear
+                .aspectRatio(1, contentMode: .fit)
+                .overlay {
+                    ancillaryView()
+                }
+        }
+    }
+
+    private func pegColor(at index: Int) -> Color {
+        switch code.kind {
+        case .master, .guess: return .primary
+        case .attempt(let matches):
+            switch matches[index] {
+            case .exact: return .green
+            case .inexact: return .yellow
+            case .noMatch: return .primary
+            }
+        }
+    }
+}
+
+private struct Letter {
+    static let shape = RoundedRectangle(cornerRadius: 10)
+    static let aspectRatio: CGFloat = 2/3
+    static let selectionPadding: CGFloat = 9
+    static let selectionColor = Color(hue: 148/360, saturation: 0, brightness: 0.8)
+}
---
+++ 2025/Word Breaker/Word Breaker/MatchMarkersView.swift
@@ -0,0 +1,66 @@
+//
+//  MatchMarkers.swift
+//  
+//
+//  Created by Alvaro Orellana on 30-08-26.
+//
+
+
+import SwiftUI
+
+struct MatchMarkersView: View {
+    let matches: [Match]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                matchMarker(pegNumber: 0)
+                matchMarker(pegNumber: 1)
+                matchMarker(pegNumber: 2)
+            }
+            HStack {
+                matchMarker(pegNumber: 3)
+                matchMarker(pegNumber: 4)
+                matchMarker(pegNumber: 5)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func matchMarker(pegNumber: Int) -> some View {
+        let exactCount = matches.count(where: { $0 == .exact})
+        let foundCount = matches.count(where: { $0 != .noMatch})
+        Circle()
+            .fill(exactCount > pegNumber ? Color.primary : Color.clear)
+            .strokeBorder(foundCount > pegNumber ? Color.primary : Color.clear, lineWidth: 2)
+            .aspectRatio(1, contentMode: .fit)
+    }
+}
+
+
+#Preview {
+    MatchMarkersPreview(matches: [.inexact,])
+    MatchMarkersPreview(matches: [.exact, .inexact, .noMatch,])
+    MatchMarkersPreview(matches: [.exact, .inexact, .inexact])
+    MatchMarkersPreview(matches: [.exact, .inexact, .inexact, .inexact ])
+    MatchMarkersPreview(matches: [.exact, .exact, .inexact, .inexact, .inexact])
+    MatchMarkersPreview(matches: [.exact, .exact, .inexact, .inexact, .inexact, .noMatch])
+}
+
+private struct MatchMarkersPreview: View {
+    let pegSize: CGFloat = 47
+    let matches: [Match]
+    
+    var body: some View {
+        HStack {
+            ForEach(matches.indices, id: \.self) { _ in
+                Circle()
+            }
+            MatchMarkersView(matches: matches)
+            Spacer()
+        }
+        .frame(height: pegSize)
+        .padding(5)
+    }
+}
+
